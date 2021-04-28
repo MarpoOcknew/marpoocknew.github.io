e88ffaeca258f60cf4cd0692e207478500323ad9
Martin Pollock <dj-smart@hotmail.co.uk>
2021-04-28 22:04:46 +0100

    Add portfolio section to cms

--- themes/vuetober/src/views/Home.vue
+++ themes/vuetober/src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
-    <div class="home">
-        <HelloWorld/>
+    <div>
+        <v-parallax dark src="https://i.pinimg.com/originals/3b/8a/d2/3b8ad2c7b1be2caf24321c852103598a.jpg" height="800" class="pa-5">
+            <HelloWorld/>
+        </v-parallax>
+
+        <div class="home">
+            <HelloWorld/>
+        </div>
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
