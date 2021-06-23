829c896a9989acc6aa53d0ca06f0680d871b21ac
Martin Pollock <martin@viacreative.co.uk>
2021-06-23 12:40:10 +0100

    Add test card

--- themes/vuetober/src/views/Home.vue
+++ themes/vuetober/src/views/Home.vue
@@ -3,10 +3,6 @@
         <v-parallax dark src="https://i.pinimg.com/originals/3b/8a/d2/3b8ad2c7b1be2caf24321c852103598a.jpg" height="800" class="pa-5">
             <HelloWorld/>
         </v-parallax>
-
-        <div class="home">
-            <HelloWorld/>
-        </div>
     </div>
 </template>
 
